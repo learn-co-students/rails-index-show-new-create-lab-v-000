129500c3765ea4b0a78e5ddecaf83ed09d988d6b
shinyrachel <rachel.kory@gmail.com>
2016-03-21 18:30:44 -0400

    coupon spec passing

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,7 @@
 class CouponsController < ApplicationController
 
   def index
+    @coupons = Coupon.all
   end
 
   def new
@@ -12,6 +13,7 @@ class CouponsController < ApplicationController
   end
 
   def show
+    @coupon = Coupon.find(params[:id])
   end
 
 end
