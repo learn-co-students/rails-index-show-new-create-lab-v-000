09e9f437d3d352aa27c96d92c03addddb3c735b8
Lady Codington <myemailforcoding@gmail.com>
2016-04-02 22:21:17 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160403020109) do
+
+  create_table "coupons", force: :cascade do |t|
+    t.text "coupon_code"
+    t.text "store"
+  end
 
 end
