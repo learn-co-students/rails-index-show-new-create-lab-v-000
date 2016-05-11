fba39886ae7ccdfe6caabe45977cab32da910695
adriandave13 <adrian@paradoxlogic.net>
2016-05-11 11:45:37 -0400

    Done.

---
+++ db/migrate/01_create_coupons.rb
@@ -0,0 +1,14 @@
+class CreateCoupons < ActiveRecord::Migration
+  def up
+    create_table :coupons do |t|
+      t.string :coupon_code
+      t.string :store
+
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :coupons
+  end
+end
