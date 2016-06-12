538b1cfbcbf2d4443abef52325e2cf4017085bd2
smithWEBtek <brad@smithwebtek.com>
2016-06-12 09:03:30 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -21,7 +21,8 @@ class CouponsController < ApplicationController
 
   def create
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
-    redirect_to "/coupons/#{@coupon.id}"
+    #redirect_to "/coupons/#{@coupon.id}"
+    redirect_to coupon_path(@coupon)
   end
 
   def destroy
