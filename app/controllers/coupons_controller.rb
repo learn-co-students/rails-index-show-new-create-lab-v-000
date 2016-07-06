7a06a0657a140cde99e87c27d3defa2be417ffc2
kade042 <dereje88@gmail.com>
2016-07-06 00:52:33 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,6 @@
 class CouponsController < ApplicationController
-  before_action :set_coupon, only: :show
+  before_action :find_coupon, only: [:show, :edit, :delete, :update]
+  before_action :coupon_params, only: :update
   def index
     @coupon = Coupon.all
   end
@@ -10,6 +11,24 @@ class CouponsController < ApplicationController
   def new
   end
 
+  def edit
+
+  end
+
+  def update
+
+    if @coupon.update!(coupon_params)
+      redirect_to coupon_path(@coupon)
+    else
+      redirect_to coupon_path
+    end
+  end
+
+  def delete
+    @coupon.destroy
+    redirect_to coupon_path
+  end
+
   def create
     @coupon = Coupon.create!(coupon_code: params[:coupon_code], store: params[:store])
     @coupon.save
@@ -17,7 +36,11 @@ class CouponsController < ApplicationController
   end
 
   private
-    def set_coupon
+    def find_coupon
       @coupon = Coupon.find(params[:id])
+    end
+
+    def coupon_params
+      params.require(:coupon).permit(:coupon_code, :store)
     end
 end
