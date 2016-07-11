0162417ada2cbf22e38a129314e4ede4f29bfacb
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-07-11 14:33:00 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+coupons = Coupon.create([{coupon_code: 'abc1', store: "Store1"}, {coupon_code: 'def2', store: "Store2"}])
