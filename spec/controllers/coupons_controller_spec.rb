852d9df4e4fcd9912bd3df73a59f727965f958bc
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-07-26 17:17:04 +0000

    Done.

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -9,7 +9,7 @@ describe CouponsController do
           post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
         }.to change(Coupon,:count).by(1)
       end
-      
+
       it "redirects to the new coupon" do
         post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
         expect(response).to redirect_to Coupon.last
@@ -18,3 +18,13 @@ describe CouponsController do
   end
 
 end
+
+# Prefix Verb   URI Pattern                 Controller#Action                                                                                                                           
+#     coupons GET    /coupons(.:format)          coupons#index
+#             POST   /coupons(.:format)          coupons#create
+#  new_coupon GET    /coupons/new(.:format)      coupons#new
+# edit_coupon GET    /coupons/:id/edit(.:format) coupons#edit
+#      coupon GET    /coupons/:id(.:format)      coupons#show
+#             PATCH  /coupons/:id(.:format)      coupons#update
+#             PUT    /coupons/:id(.:format)      coupons#update
+#             DELETE /coupons/:id(.:format)      coupons#destroy
