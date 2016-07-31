7de183f0fcc51e35eff738cbddf3d8e32ec03920
Adam Taitano <adam.taitano@gmail.com>
2016-07-31 13:03:37 -0700

    Update coupons_controller.rb

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,24 @@
-class CouponController < ApplicationController
+class CouponsController < ApplicationController
+
   def index
     @coupons = Coupon.all
   end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(
+      {
+        coupon_code: params[:coupon_code],
+        store: params[:store]
+      }
+    )
+    redirect_to coupon_path(@coupon)
+  end
+
 end
