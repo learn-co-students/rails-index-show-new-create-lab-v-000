3499fc7765be6e601d51855b6aa0c8361b6de381
steveafrost <ffrostt@gmail.com>
2016-08-10 23:24:28 -0400

    That link_to instance, instance_path is magic. Rails is magic.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  Coupon.create(
+    coupon_code: Faker::Code.isbn,
+    store: Faker::Company.name
+  )
+end
