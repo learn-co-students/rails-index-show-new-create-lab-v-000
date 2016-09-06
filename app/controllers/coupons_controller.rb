48ae6b36c23d6f67f38fa6eb659f9375170ca29c
KerryAlsace <kerrydougherty@gmail.com>
2016-09-06 17:01:12 -0400

    pass spec/features/coupon_spec.rb

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,4 +7,11 @@ class CouponsController < ApplicationController
   def index
     @coupons = Coupon.all
   end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  resources :coupons, only: [:index, :create]
-
-  # get '/coupons' => 'coupons#index'
-  # get '/coupons/create' => 'coupons#create' # needs to go above /:id
-  get '/coupons/:id' => 'coupons#show', as: :coupon
+  resources :coupons, only: [:index, :create, :show, :new]
 end
-
-# Prefix Verb URI Pattern               Controller#Action
-#        coupons GET  /coupons(.:format)        coupons#index
-#                GET  /coupons/:id(.:format)    coupons#show
-# coupons_create GET  /coupons/create(.:format) coupons#create
