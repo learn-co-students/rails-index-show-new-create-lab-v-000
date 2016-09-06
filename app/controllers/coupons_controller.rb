002d6ea5d9e488a81cbbe121b6c85d1b27641b3f
KerryAlsace <kerrydougherty@gmail.com>
2016-09-06 16:52:53 -0400

    pass spec/controllers/coupons_controller_spec.rb

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,10 @@
+class CouponsController < ApplicationController
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
+
+  def index
+    @coupons = Coupon.all
+  end
+end
