07f7c168f960df70236b45f3b6df9bfeea61e89f
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-20 22:22:30 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,9 +5,12 @@ class CouponsController < ApplicationController
   end
 
   def new
+    @coupon = Coupon.find(params[:id])
   end
 
   def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
   end
 
   def show
