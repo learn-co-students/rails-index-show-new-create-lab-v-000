a208aa3df4d5106aea3fe6e37518bc34dbac536b
github username <github email address>
2016-09-28 00:23:47 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,20 @@
+class CouponsController < ApplicationController
+
+def new
+end
+
+def index
+  @coupons = Coupon.all
+end
+
+def show
+  @coupon = Coupon.find(params[:id])
+end
+
+def create
+  @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+  redirect_to coupon_path(@coupon)
+end
+
+
+end
