163d21dcf631f1a1357534597fe56bbafd2797a4
Samuel Lair <lair001@gmail.com>
2016-10-10 04:33:50 +0000

    Done.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -8,4 +8,13 @@ describe Coupon do
   it 'can be created' do
     expect(@coupon).to be_valid
   end
+
+  it 'knows its coupon code' do
+    expect(@coupon.coupon_code).to eq("FREESTUFF")
+  end
+
+  it 'knows its store' do
+    expect(@coupon.store).to eq("Chipotle")
+  end
+
 end
