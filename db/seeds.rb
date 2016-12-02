ad1576cac716ba7d77df3efa4fabc6f9aa91d260
Aya Fushimi <aya.fushimi@gmail.com>
2016-12-02 14:43:23 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Coupon.create(coupon_code: "ASD123", store: "Chipotle")
+Coupon.create(coupon_code: "XYZ098", store: "Jamba")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
