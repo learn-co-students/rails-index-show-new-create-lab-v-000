4b3d992dead4d21e82fe12d59646d01e0fffa3ff
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-03-24 15:03:16 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,18 @@
+class CouponsController < ApplicationController
+    
+    def create
+        @coupon = Coupon.create(:coupon_code => params[:coupon_code], :store => params[:store])
+        redirect_to coupon_path(@coupon)
+    end
+    
+    def new
+    end
+    
+    def show
+        @coupon = Coupon.find(params[:id])
+    end
+    
+    def index
+        @coupons = Coupon.all
+    end
+end
