b7dcd881ee74c707ff2d10a93dbdde5ba6e4517d
Howard DeVennish <howarddevennish@gmail.com>
2017-03-28 14:19:58 +0000

    move show route to resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  resources :coupons, only: [:index, :new, :create]
-  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
+  resources :coupons, only: [:index, :new, :create, :show]
+  # get '/coupon/:id', to: 'coupons#show', as: 'coupon'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
