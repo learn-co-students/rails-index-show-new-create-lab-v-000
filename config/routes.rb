4093bacd80924a7c379dcf484ae2f33bde917ba0
Howard DeVennish <howarddevennish@gmail.com>
2017-03-28 02:19:35 +0000

    draw coupon routes and build create method

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  resources :students, only: [:index, :new, :create]
+  get '/student/:id', to: 'students#show', as: 'student'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
