0d4cdc9709d28c6fbbeaf66d262165e8128a86f2
anytimesoon <damien@delartigue.co.uk>
2017-04-24 18:45:07 +0100

    new and create routes working

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,6 +10,8 @@ class CouponsController < ApplicationController
 	end
 
 	def create
+		coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+		redirect_to coupon_path(coupon)
 	end
 	
 end
