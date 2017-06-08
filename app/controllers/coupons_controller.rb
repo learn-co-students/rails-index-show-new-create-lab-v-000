27bbbcea92e1cedc790215d7a5a984e8542b7c77
Scott Atkins <satkins16@gmail.com>
2017-06-08 13:05:25 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,17 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def created
+  end
+
+end
