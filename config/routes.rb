700fb72c97f5a892450c8bacf3faa6ad7ede49ab
John Fewell <fewell@gmail.com>
2017-07-06 19:05:42 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :coupons, only: [:index, :show, :new, :create]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
