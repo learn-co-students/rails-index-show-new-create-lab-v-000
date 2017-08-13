b81e4b5073be808e9b5bc5bebc4763e4c18b0917
Terry Blue <terryblue@comcast.net>
2017-08-13 01:36:04 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :couponss, only: [:index, :show, :new, :create]
+  resources :coupons, only: [:index, :show, :new, :create]
 
   # Example resource route with options:
   #   resources :products do
