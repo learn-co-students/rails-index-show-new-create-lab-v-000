48f40598d549cfef8861171e51ec5913c8c74d1b
Soumya Veer <veer.soumya@gmail.com>
2017-09-16 05:43:49 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new(coupon_code: params[:coupon_code], store: params[:store])
+    if @coupon.save
+      redirect_to coupon_path(@coupon)
+    else
+      redirect_to coupons_path
+    end
+  end
+end
