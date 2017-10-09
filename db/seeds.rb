cf8dc7ecf06965dda4ea7dd8820a401a5dd15621
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-10-09 05:48:28 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def rand_word
+  (0...8).map { (65 + rand(26)).chr }.join
+end
+
+30.times do
+  Coupon.create(coupon_code:rand_word, store:rand_word)
+end
