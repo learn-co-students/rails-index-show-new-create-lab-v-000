8bdea779643e3f89b09be429642741ca5ab7b4fd
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 16:54:53 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,19 @@
+class CouponsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+end
