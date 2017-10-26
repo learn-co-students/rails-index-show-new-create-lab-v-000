e257b4058aaf0223d201d8225303cff336b4e0d0
lpassamano <leighayna.josele@gmail.com>
2017-10-26 12:31:41 -0400

    stubbed coupon controller actions

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,16 @@
 class CouponsController < ApplicationController
-end 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+end
