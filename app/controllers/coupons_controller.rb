715b0576f23167c8c0e05165152b3492b0fadb95
lpassamano <leighayna.josele@gmail.com>
2017-10-26 12:39:57 -0400

    added new coupon form and create controller action

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,6 +11,7 @@ class CouponsController < ApplicationController
   end
 
   def create
-
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
   end
 end
