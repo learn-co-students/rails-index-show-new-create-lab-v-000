8c111d280ddf48dd7221140bd7b3c039de57fbef
Sam Marshall <samuel.r.marshall@gmail.com>
2017-12-06 22:47:04 -0500

    for Learn.co question

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,16 @@
+class CouponsController < ApplicationController
+  def index
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(store: params[:store], coupon_code: params[:coupon_code])
+    redirect_to coupon_path(@coupon)
+  end
+end
--- spec/views/coupons/show.html.erb_spec.rb
+++ spec/views/coupons/show.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Coupon, type: :model do
+RSpec.describe "coupons/show.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
