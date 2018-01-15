808f24dd8c25f6506ca78823a535692d58dacbc5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-15 14:16:27 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,8 +11,11 @@ class CouponsController < ApplicationController
   end
 
   def create
-
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
     redirect_to coupon_path(@coupon)
   end
-  
+
 end
