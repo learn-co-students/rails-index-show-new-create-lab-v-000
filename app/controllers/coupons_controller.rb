580812910e030cd8b73c6ec4f862db3984ee2dc9
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-15 14:04:31 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,18 @@
 class CouponsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @coupon = Coupon.find_by_id(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+
+    redirect_to coupon_path(@coupon)
+  end
+  
 end
