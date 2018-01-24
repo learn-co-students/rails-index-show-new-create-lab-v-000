5690efd8b84c6cdd7e4873a92b6eacc36fec4fa8
Hannah Carnes <hecarnes@gmail.com>
2018-01-24 14:55:10 -0500

    5 tests left

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,24 @@
 class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+    @coupon = Coupon.new(store: params[:store], coupon_code: params[:coupon_code])
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.store = params[:store]
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
 end
+
+
