37afeb4b3260f1a587b3cd0f94894ad3761fba8f
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-01-31 07:05:15 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,19 @@
 class CouponsController < ActionController::Base
 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
 
 end
