ba87da39ee27c73ed03d0cd3c957f63c6cea6c1a
Brittany Morris <brittmmendez@gmail.com>
2018-02-07 17:22:37 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,16 @@ class CouponsController < ApplicationController
     @coupons=Coupon.all
   end
 
+  def show
+    @coupon=Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon=Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
+
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,6 @@
 class Coupon < ActiveRecord::Base
+  def to_s
+    self.coupon_code + " - " + self.store
+  end
 
 end
