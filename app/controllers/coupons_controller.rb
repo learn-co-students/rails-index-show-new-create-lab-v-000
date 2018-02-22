1ce4a1369c835870befc88cfa1ebe41811a6b59e
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-02-22 22:56:12 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,13 @@
+class CouponsController < ApplicationController
+      def index
+    @coupons = Coupon.all
+  end
+  
+  def new
+  end
+  
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+  redirect_to coupons_path
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180222223306) do
+
+  create_table "coupons", force: :cascade do |t|
+  end
 
 end
