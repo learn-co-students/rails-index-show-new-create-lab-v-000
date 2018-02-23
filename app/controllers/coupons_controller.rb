df0302fcc943a25819525d87cb415352f57541ac
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-02-23 00:45:25 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,7 +7,7 @@ class CouponsController < ApplicationController
   end
   
   def create
-    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
   redirect_to coupons_path
   end
 end
