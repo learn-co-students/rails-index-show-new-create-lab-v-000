76c9f478267cf7f7e1326bc8937b1a48cab97fd3
Katrina Brinson <kbrinso@gmail.com>
2018-03-30 17:53:07 +0000

    ran into bug with ide

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,21 +1,5 @@
-class Coupon < ApplicationController
-  def index
-   @coupons = Coupon.all
- end
-
- def show
-   @coupon = Coupon.find(params[:id])
- end
-
- def new
-   @coupon = Coupon.new
- end
-
- def create
-   @coupon = Coupon.new
-   @coupon.coupon_code = params[:coupon_code]
-   @coupon.store = params[:store]
-   @coupon.save
-   redirect_to coupon_path(@coupon)
+class Coupon < ActiveRecord::Base
+  def to_s
+    self.coupon_code + " " + self.store
   end
 end
