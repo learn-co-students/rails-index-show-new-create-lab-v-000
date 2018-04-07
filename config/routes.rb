688a082d9748ab58fcb8a95753144d897375f295
Chely Ho <adept_inept@yahoo.com>
2018-04-07 06:15:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :coupons, only: [:index, :show, :create]
+  resources :coupons, only: [:index, :show, :new, :create]
 end
