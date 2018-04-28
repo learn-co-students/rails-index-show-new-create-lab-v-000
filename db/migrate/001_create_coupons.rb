598c545ef6b1678e20f514227f01536fef41286d
AReid987 <read.musik@gmail.com>
2018-04-27 21:52:01 -0500

    Done.

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -1,10 +1,10 @@
 class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |t|
-      t.string :coupon_code, null: false
-      t.string :store, null: false
+      t.string :coupon_code
+      t.string :store
 
-      t.timestamps null: false
+      #t.timestamps null: false
     end
   end
 end
