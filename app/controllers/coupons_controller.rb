4f941de6da75369a4357d4aea218b3da4ee641dd
Eden <emesfun@outlook.com>
2018-04-28 18:44:21 -0400

    completed lab

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -17,6 +17,6 @@ class CouponsController < ApplicationController
         @coupon.store = params["coupon"]["store"]
         @coupon.save
 
-        redirect_to coupons_path(@coupon)
+        redirect_to coupon_path(@coupon)
     end
 end
