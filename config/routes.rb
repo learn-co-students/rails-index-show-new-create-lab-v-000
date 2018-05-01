cbf9348477614e0b18e98f0730b3d9023e142df2
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-05-01 10:52:59 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :coupons, only: [:index]
+  resources :coupons, only: [:index, :new, :create, :show]
 end
