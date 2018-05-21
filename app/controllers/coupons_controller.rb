d6d886a8424b4606edfb6bd691ce933f7976e70a
AlexFriedland <friedlandaj@gmail.com>
2018-05-21 10:05:04 +0200

    created controller

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,13 @@
+class CouponsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'coupons/index'
+
+  get 'coupons/new'
+
+  get 'coupons/show'
+
+  get 'coupons/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
