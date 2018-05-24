cc0929f9383b593b6f39d7c3652712cf8435f810
ElvinGarcia <dimix181@gmail.com>
2018-05-23 22:42:11 -0400

    all test are passing

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,15 +1,28 @@
 class CouponsController < ApplicationController
+  before_action :all, only: [:index, :show]
 
   def index
-    binding.pry
+    @coupons
   end
 
   def new
   end
 
-  def create
-    binding.pry
+  def show
+     @coupon = @coupons.find(params[:id])
   end
 
+  def create
+    @coupon = Coupon.create(store:params[:coupon][:store], coupon_code:params[:coupon][:coupon_code])
+    redirect_to coupon_path(@coupon)
+  end
+
+
+
+private
+
+  def all
+      @coupons ||= Coupon.all
+  end
 
 end
