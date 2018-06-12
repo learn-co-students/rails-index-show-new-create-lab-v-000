4e0699c63571251bfefc6335ae0045745cd8533e
Emily Jirles <emily.jirles@gmail.com>
2018-06-12 15:01:59 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "FREETHINGSNOW", store: "Forever XXI")
+Coupon.create(coupon_code: "COFFEEFORDAYS", store: "Peets")
+Coupon.create(coupon_code: "YOTENGOHAMBRE", store: "Taco Bell")
