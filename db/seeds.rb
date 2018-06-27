c3771a41afb3d2a1b20038686d744531f369ec41
Nicholas Powers <njpowers7915@gmail.com>
2018-06-27 16:35:42 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.new(coupon_code: 'abcdefg', store: 'CVS - Wyckoff')
