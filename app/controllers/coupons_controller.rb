2c37bc6cb32f9edef63acf92d274cde23bf9640c
Vanessa Toro <vanetoro84@gmail.com>
2018-06-29 19:37:17 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,11 +1,15 @@
+require 'pry'
 class CouponsController < ApplicationController
 
+def index
+  @coupons = Coupon.all
+end
  def show
-   @coupons = Coupon.all
+   @coupon = Coupon.find(params[:id])
  end
 
  def create
-   @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+   @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
    redirect_to coupon_path(@coupon)
  end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
- get 'coupons/:id' => "coupons#show"
+ get '/coupons/:id' => "coupons#show", as: 'coupon'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
