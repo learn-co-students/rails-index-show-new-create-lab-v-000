d24476c6b8693a96a8a407cb70604947a4ec84a2
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-02 16:01:55 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,19 @@
 class CouponsController < ApplicationController
 
-  def create
+  def index
+    @coupons = Coupon.all
   end
-  
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new(params[:coupon_code][:store])
+    @coupon.save
+  end
+
 end
