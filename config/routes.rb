dd2d5373be26a92c382d2e892a2145228349b7ef
Zac Willmington <zac.willmington@gmail.com>
2018-07-05 19:00:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :coupons, only: [:index, :new, :create]
-  get '/coupons/:id', to: 'coupons#show', as: 'student'
+  get '/coupons/:id', to: 'coupons#show', as: 'coupon'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
