56df90d9c12732bfbe7fc349b2fa512cab2e052f
taryn yaeger <tsyaeger@gmail.com>
2018-07-25 17:44:41 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :coupons, only: [:index, :new, :create]
-  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
+  resources :coupons, only: [:index, :new, :create, :show]
+  # get '/coupon/:id', to: 'coupons#show', as: 'coupon'
 end
 
   
