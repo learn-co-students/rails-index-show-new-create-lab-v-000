7c309276a807092ca3cb0eafdc829ceaa3c6bf41
Eddie Moon <eddiemoonie@gmail.com>
2018-07-27 22:31:42 +0000

    Index New Show Coupon

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :coupons, only: [:index, :new, :create]
-  get '/coupons/:id', to: 'coupons#show', as: 'coupon'
+  resources :coupons, only: [:index, :new, :show, :create]
 end
