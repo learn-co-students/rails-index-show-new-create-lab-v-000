2b723e269adf79dc90611032bd0c9c877690aa4a
netzay <netzelally@gmail.com>
2018-07-30 14:35:10 -0600

    Done.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,3 @@
 class Coupon < ActiveRecord::Base
-  def to_s
-    self.coupon_code + " " + self.store
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :coupons, only: [:index, :new, :show, :create]
+  resources :coupons, only: [:index, :new, :create]
+  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
