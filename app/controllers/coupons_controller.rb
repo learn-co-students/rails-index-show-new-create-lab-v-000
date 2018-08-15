d24b68e8c2ebf303bc74b170ccb7b2b417fde883
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-15 02:37:11 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,20 @@
-class CouponControllerController < ApplicationController
+
+class CouponsController < ApplicationController
+  
+  def index
+    @coupons = Coupon.all 
+  end
+  
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+  
+  def new 
+  end
+  
+  def create
+    @coupon = Coupon.create
+    redirect_to coupon_path(@coupon)
+  end
+  
 end
