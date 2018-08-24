de8d434e48bbe3c04230756091f0fa6ad140f7e2
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-24 23:04:24 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,13 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+end
