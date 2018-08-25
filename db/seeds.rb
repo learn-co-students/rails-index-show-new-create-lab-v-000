b8a1f22353e9b4e7c4a70a5036eb751c3addeacb
Carley Tripp <carley.tripp@outlook.com>
2018-08-25 04:06:07 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: 'ERDFA341D', store: 'Target')
+Coupon.create(coupon_code: 'DFAK43H5J', store: 'Target')
+Coupon.create(coupon_code: 'DFAJH5K1K', store: 'Target')
+Coupon.create(coupon_code: 'MGR354254', store: 'Pizza Hut')
+Coupon.create(coupon_code: 'IAMPOORBB', store: 'Walmart')
