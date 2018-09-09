884bb8754bc05e6c5780d2341e285c952ff0b5af
fkmccallion <fkmccallion@gmail.com>
2018-09-09 12:06:29 -0700

    remove unnecessary save

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,7 +13,7 @@ class CouponsController < ApplicationController
         
     def create
         @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
-        @coupon.save
+        #@coupon.save
 
         redirect_to coupon_path(@coupon)
     end
