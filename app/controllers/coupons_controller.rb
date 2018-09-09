c5a58689121fb5237851f0dd85dd539f7b247f79
fkmccallion <fkmccallion@gmail.com>
2018-09-09 11:16:50 -0700

    save work before closing vm

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,15 @@
+class CouponsController < ApplicationController
+
+    def index
+
+    end
+        
+    def create
+        @coupon = Coupon.new
+        @coupon.coupon_code = params[:coupon_code]
+        @coupon.store = params[:store]
+        @coupon.save
+
+        redirect_to coupon_path(@coupon)
+    end
+end
