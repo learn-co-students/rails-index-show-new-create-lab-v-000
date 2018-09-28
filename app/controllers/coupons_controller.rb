2d04191961785279fab616d8e3b49b19142de46e
Nick-Damico <nickalan82@icloud.com>
2018-09-28 14:05:47 -0400

    views index, new built

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,8 @@
 class CouponsController < ApplicationController
   def index
+    @coupons = Coupon.all
+  end
+
+  def new
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Coupon.create(coupon_code: "ASD123", store: "Chipotle")
+# Coupon.create(coupon_code: "XYZ098", store: "Jamba")
+Coupon.create(coupon_code: "GMLOT1", store: "Steam")
