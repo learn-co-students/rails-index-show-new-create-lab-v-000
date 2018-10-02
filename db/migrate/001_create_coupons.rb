492503ab8146bcfbfd6db9f75a77b96dfbeb9ab5
Joana Palero <joana.palero24@gmail.com>
2018-10-02 12:23:46 +0000

    Done.

---
+++ db/migrate/001_create_coupons.rb
@@ -0,0 +1,9 @@
+class CreateCoupons < ActiveRecord::Migration
+  def change
+    create_table :coupons do |t|
+      t.string :coupon_code, null: false
+      t.string :store, null: false
+      t.timestamps null: false
+    end
+  end
+end
