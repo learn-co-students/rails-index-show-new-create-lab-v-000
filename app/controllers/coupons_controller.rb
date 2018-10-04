17f1602ed14493e7f2335506d0a9dcece65d2a8f
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-10-04 14:33:22 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,11 +1,11 @@
 class CouponsController < ApplicationController
   def index
-
+    @coupons = Coupon.all
   end
   def new
 
   end
   def create
-    
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
   end
 end
