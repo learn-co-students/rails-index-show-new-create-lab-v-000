1bad9bccf4c0a397a6e5eac9c103dfd162b9ee26
FKRauber <Fabienne.Rauber@gmail.com>
2018-10-26 18:02:46 -0400

    created folder in views ; moved index and created new and show files ; updated create path in controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,7 +12,7 @@ class CouponsController < ApplicationController
 
 	def create
 		@coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
-		redirect_to coupon_path
+		redirect_to coupon_path(@coupon)
 	end
 
 end
