936fbbe78236c327b37a35cb1b8966da7a512a47
FKRauber <Fabienne.Rauber@gmail.com>
2018-10-26 18:21:46 -0400

    filled out views ; updated create method ; corrected error in show method

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,14 +4,17 @@ class CouponsController < ApplicationController
 	end
 
 	def show
-		@coupons = Coupon.find(params[:id])
+		@coupon = Coupon.find(params[:id])
 	end
 
 	def new
 	end
 
 	def create
-		@coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+		@coupon = Coupon.new
+		@coupon[:coupon_code] = params[:coupon][:coupon_code] 
+		@coupon[:store] = params[:coupon][:store]
+		@coupon.save
 		redirect_to coupon_path(@coupon)
 	end
 
