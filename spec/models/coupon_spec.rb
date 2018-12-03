a7f35728e401e1b5cb91884b15b00caa707d6c13
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-12-03 22:48:53 +0000

    Done.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
-describe Coupon do
-  before(:each) do
-    @coupon = Coupon.create!(coupon_code: "FREESTUFF", store: "Chipotle")
-  end
-
-  it 'can be created' do
-    expect(@coupon).to be_valid
-  end
+RSpec.describe Coupon, type: :model do
 end
