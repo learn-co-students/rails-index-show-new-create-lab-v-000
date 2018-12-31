3fb3533f95d69b6b19475cb6d6bb2ed00949b12b
Judy McGoldrick <jjeanius@verizon.net>
2018-12-31 04:37:58 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,25 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to @coupon
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+
+
+end
