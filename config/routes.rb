b64f918ddf3937e69e17d463d576289c11e924db
Nate Dietz <nsdietz@gmail.com>
2019-02-10 20:20:32 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :coupons, ony: [:index, :show]
+  post 'coupons/new', to: 'coupons#create'
 end
