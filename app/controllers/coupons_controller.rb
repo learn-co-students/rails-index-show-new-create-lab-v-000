a9a7c59bbe4640084910b173c1c8b9dcee69cc1e
Ashley Wong <ashwong04@gmail.com>
2019-03-29 16:43:07 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,16 @@
 class CouponsController < ApplicationController
 
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+  end
 
 end
