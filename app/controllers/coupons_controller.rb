c82f3bbcd17790d4233ed5015089df6909dfb417
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-04 15:25:28 -0400

    Specify attributes for create action

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,7 +6,7 @@ class CouponsController < ApplicationController
   end
 
   def create
-    @coupon = Coupon.create(params)
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
     redirect_to coupon_path(@coupon)
   end
 
