3047b933e0cca29051242d0a1d50f8c7ecca7893
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-04 14:56:17 -0400

    Add methods to coupons controller
    I added the index action with the ability to access all instances of the Coupon class so it can list all the coupons in the index view page. I added the create action to take in the params from the user-submitted 'Create New Coupon' form and persist that information as a new record in the database while also creating a new Coupon object. I also set a private method to find a coupon by its ID.
    
    I think the show action will still work properly even though I didn't specifically declare it in my controller because of **Rails magic**. :P We'll find out.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,17 @@
 class CouponsController < ApplicationController
-  
+  before_action :set_coupon, only: [:show]
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def create
+    @coupon = Coupon.create(params)
+    redirect_to coupon_path(@coupon)
+  end
+
+  private
+    def set_coupon
+      @coupon = Coupon.find(params[:id])
+    end
 end
