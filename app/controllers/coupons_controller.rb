16971a757aaddaf22c38b1d6cbd78a6678382d70
sdcro <sdcrouse91@gmail.com>
2019-11-18 00:38:47 -0800

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,4 +9,13 @@ class CouponsController < ApplicationController
 
   def new
   end
+
+  def create
+    coupon = Coupon.new
+    coupon.coupon_code = params[:coupon][:coupon_code]
+    coupon.store = params[:coupon][:store]
+    coupon.save
+
+    redirect_to coupon_path(coupon)
+  end
 end
