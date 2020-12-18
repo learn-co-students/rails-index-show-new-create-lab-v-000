adeb79b7fdb24a71ba17565220ccd2236cbdfd1e
lrh2daizzo <herrera.lrh@gmail.com>
2020-12-17 18:17:08 -0600

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,6 @@
 class CouponsController < ApplicationController
     def index
-        @coupons = Coupon.all
+        @coupons = Coupon.all 
     end
 
     def show
@@ -14,7 +14,8 @@ class CouponsController < ApplicationController
         @coupon = Coupon.new 
         @coupon.coupon_code = params[:coupon][:coupon_code]
         @coupon.store = params[:coupon][:store]
-        @coupon.save
+        @coupon.save 
         redirect_to coupon_path(@coupon)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-    resources :coupons, only: [:index, :show, :create, :new]
+  resources :coupons, only: [:index, :show, :new, :create]
   # Example resource route with options:
   #   resources :products do
   #     member do
