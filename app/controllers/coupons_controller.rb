4e3487e5395d646b9e51856c4fc999d0b834961d
Jesse Wenger <wengerj@gmail.com>
2021-04-26 22:37:59 +0000

    add paths

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,16 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+  end
+end
